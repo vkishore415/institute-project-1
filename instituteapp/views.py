01a1172cc33ba2877863782b33c311a46cfdafa1
GarishGcv <harish.gcv@gmail.com>
2019-05-31 16:27:19 +0530

    institute project added to git

--- instituteapp/views.py
+++ instituteapp/views.py
@@ -8,6 +8,7 @@ import datetime
 #added demo to git
 #added demo from pycharm to git
 #added from git to pycharm
+#added from pyharm to git
 date1=datetime.datetime.now()
 # Create your views here.
 def main_page(request):
